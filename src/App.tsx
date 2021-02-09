fad82161f03fa03dafcdd1f19f864b48a2271222
pushkar87singh <pushkar87singh@gmail.com>
2021-02-09 13:30:32 +0530

    yes, it is working for me Thanks CodeSandbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import "./styles.css";
-//import Typography from "@material-ui/core/Typography";
+import { Typography } from "@material-ui/core";
 import HeaderBar from "./components/header/header-bar/HeaderBar";
 export default function App() {
   return (
     <div className="App">
-      {/* <Typography variant="h4">Welcome to Typescript</Typography> */}
-      <HeaderBar />
+      <HeaderBar>Welcome to My AppBar Header</HeaderBar>
+      <Typography variant="h4">Welcome to Typescript</Typography>
     </div>
   );
 }
